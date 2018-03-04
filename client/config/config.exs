21a9f7ece2873278420d4eab1f6603efddf2362c
dingo sky <paul@dingosky.com>
2018-03-04 14:38:22 -0800

    Update srpc_client

--- client/config/config.exs
+++ client/config/config.exs
@@ -2,10 +2,8 @@ use Mix.Config
 
 config :srpc_client,
   srpc_file: "priv/client.srpc",
-  srpc_transport: SrpcPoison
+  transport: SrpcPoison
 
 config :srpc_client, :server,
   host: "localhost",
   port: 8082
-
-config :srpc_poison, proxy: "http://localhost.charlesproxy.com:8888"
--- server/config/config.exs
+++ server/config/config.exs
@@ -14,7 +14,7 @@ config :kncache,
   caches: [
     srpc_exch: 30,
     srpc_nonce: 35,
-    srpc_conn: 10800,
-    srpc_reg: 10800,
-    user_data: 10800
+    srpc_conn: 3600,
+    srpc_reg: 3600,
+    user_data: 3600
   ]
