026356435d9bcf20eb67baa6d8a42f7dfff791f4
dingo sky <paul@dingosky.com>
2018-03-08 13:00:35 -0800

    Change IEx prompts

--- server/.iex.exs
+++ server/.iex.exs
@@ -1,11 +1,11 @@
 Application.put_env(:elixir, :ansi_enabled, true)
 IEx.configure(
+  inspect: [pretty: true, limit: :infinity],
   colors: [enabled: true],
   default_prompt: [
     "\e[G",    # ANSI CHA, move cursor to column 1
     :blue,
-    "server:%prefix", # IEx prompt variable
-    ">",       # plain string
+    "server \u2234", # prompt
     :reset
   ] |> IO.ANSI.format |> IO.chardata_to_string
 )
